54379129b168df124718eea0fec91d317b39dc7b
Kreoin <steamuser85@yandex.ru>
2024-07-21 13:50:39 +0300

    add url validation

--- src/main/java/org/kreoin/shorturl/entity/Url.java
+++ src/main/java/org/kreoin/shorturl/entity/Url.java
@@ -1,19 +1,21 @@
 package org.kreoin.shorturl.entity;
 
-import jakarta.persistence.*;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import lombok.Getter;
-import lombok.Setter;
+import java.time.LocalDate;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDate;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
 import static jakarta.persistence.GenerationType.IDENTITY;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -24,10 +26,12 @@ public class Url {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
-    @NotBlank
+
+    @Column(columnDefinition="TEXT")
+    @Pattern(regexp = "^https?://[0-9a-zA-Z.\\-]{0,100}/[0-9A-z?=\\-&:%#_/]{0,1500}")
     private String longUrl;
 
-    @Pattern(regexp = "[0-9a-zA-Z]{1,20}")
+    @Pattern(regexp = "[0-9a-zA-Z]{0,20}")
     private String shortUrlToken;
 
     @LastModifiedDate
